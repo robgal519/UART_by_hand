250d0830f7b29dd61fad5551ee7ba125d2ee5a6a
Robert Gałat <robgal519@gmail.com>
2020-05-24 16:11:08 +0200

    fixed documentation and formating

--- Inc/swo_logger.h
+++ Inc/swo_logger.h
@@ -3,11 +3,15 @@
 
 #include "stdint.h"
 
-
+/**
+ * @brief definition of the logger levels,
+ * The smaller value is more significant
+ * 
+ */
 typedef enum LogLevel{
-    LOG_INFO,
+    LOG_ERROR,
     LOG_WARNING,
-    LOG_ERROR
+    LOG_INFO    
 }LogLevel;
 
 static const char* const LogLevelNames[] ={
@@ -16,21 +20,40 @@ static const char* const LogLevelNames[] ={
     [LOG_ERROR]     = "[ ERROR ]   "
 };
 
+/**
+ * @brief configuration object of the logger
+ * 
+ * send  function to send one character. The interface is design to work with ITM_sendChar()
+ * level this is  level that the logger will react to
+ * 
+ */
 typedef struct LogConfiguration{
     uint32_t (*send)(uint32_t);
     LogLevel level;
 }LogConfiguration;
 
+/**
+ * @brief initialize the logger
+ * 
+ * @param configuration apply configuration 
+ * The configuration has to be valud as long as the logger is used.
+ * Because, the module keep the pointer to this config, the value can change in runtime,
+ */
 void LogInit(LogConfiguration* configuration);
 
-
-
+/**
+ * @brief write a log message
+ * 
+ * @param level see enum LogLevel
+ * @param message message to send
+ */
 void Log(LogLevel level, const char* message);
 
-
-
+/**
+ * @brief DEBUG macro has to be defined if the logger is to be used
+ * 
+ */
 #ifdef DEBUG
-
 #define LOG_INFO(message) Log(LOG_INFO, message)
 #define LOG_WARNING(message) Log(LOG_WARNING, message)
 #define LOG_ERROR(message) Log(LOG_ERROR, message)
